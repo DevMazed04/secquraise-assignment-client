0a8e39ed41ae35d26abb5e78e6a180ed4bfdf83f
DevMazed04 <mdmazedislam2684@gmail.com>
2023-01-25 17:00:11 +0600

    installed tailwind css and daisyui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+  import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Full-Stack Assignment</h1>
+      <h1 className="text-3xl font-bold mt-10">
+        Full-Stack Assignment
+      </h1>
     </div>
   );
 }
